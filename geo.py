3dd3776ad550d6700bda489df96cb6f55a804eed
HusainMiyala <98285048+HusainMiyala@users.noreply.github.com>
2022-01-23 15:57:25 -0500

    Updated Dataset to run Local

--- geo.py
+++ geo.py
@@ -2,9 +2,9 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("J:/APc_plan/Control Group/ALC/ALC TM's/Husain M/Projects/SolarOutput_Province.csv")
+df = pd.read_csv("./SolarOutput_Province.csv")
 
-map_df = gpd.read_file("J:/APc_plan/Control Group/ALC/ALC TM's/Husain M/Projects/lpr_000b16a_e.shp")
+map_df = gpd.read_file("./lpr_000b16a_e.shp")
 map_df = map_df.rename({'PRENAME':'Province'}, axis = 'columns')
 map_df = map_df.drop(['PRUID', 'PRNAME', 'PRFNAME', 'PREABBR', 'PRFABBR'], axis = 1)
 
